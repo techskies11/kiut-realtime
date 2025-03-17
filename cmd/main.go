9267109bf7f33a0a28d25e2c8920727928981942
Camilo Torres <camilotorres.9576@gmail.com>
2025-03-17 22:36:54 +0100

    added aws client integration for posttoconnection

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,53 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
 )
+
+type SimpleContext struct {
+	ConnectionID string `json:"connectionId"`
+}
+
+type BodyContext struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+}
+
+type MessageContext struct {
+	Body         BodyContext `json:"body"`
+	ConnectionId string      `json:"connectionId"`
+}
+
+var apiGatewayClient *apigatewaymanagementapi.Client
+var apiEndpoint string
+
+func init() {
+	// Set API Gateway URL from environment variable
+	apiEndpoint = os.Getenv("API_GATEWAY_ENDPOINT")
+	if apiEndpoint == "" {
+		log.Fatal("API_GATEWAY_ENDPOINT environment variable is not set")
+	}
+
+	// Load AWS SDK v2 configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("Unable to load AWS SDK config: %v", err)
+	}
+
+	// Initialize API Gateway Management API client
+	apiGatewayClient = apigatewaymanagementapi.NewFromConfig(cfg, func(o *apigatewaymanagementapi.Options) {
+		o.BaseEndpoint = aws.String(apiEndpoint)
+	})
+}
 
 func connectHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -16,7 +57,7 @@ func connectHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	var data any
+	var data SimpleContext
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
@@ -24,7 +65,7 @@ func connectHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	response := fmt.Sprintf("Received: %s", data)
+	response := fmt.Sprintf("Connecting: %s", data)
 	println(response)
 
 	w.WriteHeader(http.StatusOK)
@@ -39,7 +80,7 @@ func disconnectHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	var data any
+	var data SimpleContext
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
@@ -47,7 +88,7 @@ func disconnectHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	response := fmt.Sprintf("Received: %s", data)
+	response := fmt.Sprintf("Disconnecting: %s", data)
 	println(response)
 
 	w.WriteHeader(http.StatusOK)
@@ -62,7 +103,7 @@ func sendMessageHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	var data any
+	var data MessageContext
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
@@ -70,11 +111,35 @@ func sendMessageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	response := fmt.Sprintf("Received: %s", data)
+	response := fmt.Sprintf("I got your message!: %s", data)
 	println(response)
 
+	// Extract connectionId from data
+	connectionID := data.ConnectionId
+	message := []byte(data.Body.Message)
+	if (sendMessageToClient(connectionID, message)) != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Failed to send message"})
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"response": response})
+}
+
+func sendMessageToClient(connectionID string, message []byte) error {
+	ctx := context.TODO()
+
+	input := &apigatewaymanagementapi.PostToConnectionInput{
+		ConnectionId: aws.String(connectionID),
+		Data:         message,
+	}
+
+	_, err := apiGatewayClient.PostToConnection(ctx, input)
+	if err != nil {
+		println(err)
+		return fmt.Errorf("failed to send message: %w", err)
+	}
+	return nil
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
