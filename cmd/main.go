2b932aafa8660255e624341a207235262b4298a0
Camilo Torres <camilotorres.9576@gmail.com>
2025-03-18 07:06:53 -0500

    Feature/gateway integration (#1)
    * added aws client integration for posttoconnection
    
    * removed json responses
    
    * print headers
    
    * added more prints to debug post to connection error
    
    * working iteration with aws

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,53 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
 )
+
+type SimpleContext struct {
+	ConnectionID string `json:"connectionId"`
+}
+
+type BodyContext struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+}
+
+type MessageContext struct {
+	Body         BodyContext `json:"body"`
+	ConnectionId string      `json:"connectionId"`
+}
+
+var apiGatewayClient *apigatewaymanagementapi.Client
+var apiEndpoint string
+
+func init() {
+	// Set API Gateway URL from environment variable
+	apiEndpoint = os.Getenv("API_GATEWAY_ENDPOINT")
+	if apiEndpoint == "" {
+		log.Fatal("API_GATEWAY_ENDPOINT environment variable is not set")
+	}
+
+	// Load AWS SDK v2 configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("Unable to load AWS SDK config: %v", err)
+	}
+
+	// Initialize API Gateway Management API client
+	apiGatewayClient = apigatewaymanagementapi.NewFromConfig(cfg, func(o *apigatewaymanagementapi.Options) {
+		o.BaseEndpoint = aws.String(apiEndpoint)
+	})
+}
 
 func connectHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -14,21 +55,16 @@ func connectHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-
-	var data any
+	var data SimpleContext
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
 		return
 	}
 	defer r.Body.Close()
 
-	response := fmt.Sprintf("Received: %s", data)
-	println(response)
-
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"response": response})
 }
 
 func disconnectHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,21 +73,16 @@ func disconnectHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-
-	var data any
+	var data SimpleContext
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
 		return
 	}
 	defer r.Body.Close()
 
-	response := fmt.Sprintf("Received: %s", data)
-	println(response)
-
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"response": response})
 }
 
 func sendMessageHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,21 +91,43 @@ func sendMessageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-
-	var data any
+	var data MessageContext
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
 		return
 	}
 	defer r.Body.Close()
 
-	response := fmt.Sprintf("Received: %s", data)
-	println(response)
+	// Extract connectionId from data
+	connectionID := data.ConnectionId
+	message := fmt.Appendf(nil, "msg recieved: %s", data.Body.Message)
+
+	err := sendMessageToClient(connectionID, message)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusGone)
+		json.NewEncoder(w).Encode(map[string]string{"error": fmt.Sprintf("Failed to send message: %v", err)})
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"response": response})
+}
+
+func sendMessageToClient(connectionID string, message []byte) error {
+	ctx := context.TODO()
+
+	input := &apigatewaymanagementapi.PostToConnectionInput{
+		ConnectionId: aws.String(connectionID),
+		Data:         message,
+	}
+
+	_, err := apiGatewayClient.PostToConnection(ctx, input)
+	if err != nil {
+		return fmt.Errorf("Failed to send message: %v", err)
+	}
+	return nil
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
